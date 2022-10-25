8b4d5c0aec0b93c79047edd7d672b3daa31f66f8
Yurong Liu <r@Rains-MacBook-Pro.local>
2022-10-25 00:29:16 -0400

    Add files

--- main_chain_disjoint.py
+++ main_chain_disjoint.py
@@ -5,7 +5,7 @@ from acyclic_join import *
 from build_hash import *
 import pickle
 # from sample_from_disjoint import *
-from sample_disjoint_3 import *
+from sample_disjoint_1 import *
 from sample_union_bernoulli import *
 
 def fixed_sample(table, fixed, scale):
@@ -58,89 +58,89 @@ def main():
     k = 0
 
     # Qx: nation, supplier, customer, orders, lineitem 
-    nation = pd.read_table('./tpch_3/nation.tbl', index_col=False, names=['NationKey','NationName','RegionKey','Comment'], delimiter = '|').iloc[:, :-1]
-    supplier = pd.read_table('./tpch_3/supplier.tbl', index_col=False, names=['SuppKey','SuppName','Address','NationKey','Phone','Acctbl','Comment'], delimiter = '|').iloc[:, :-1]
-    customer = pd.read_table('./tpch_3/customer.tbl', index_col=False, names=['CustKey','CustName','Address','NationKey','Phone','Acctbal','MktSegment','Comment'], delimiter = '|').iloc[:, :-1]
-    orders = pd.read_table('./tpch_3/orders.tbl', index_col=False, names=['OrderKey','CustKey','OrderStatus','TotalPrice','OrderDate','OrderPriority','Clerk','ShipPriority','Comment'], delimiter = '|').iloc[:, :-1]
-    lineitem = pd.read_table('./tpch_3/lineitem.tbl', index_col=False, names=['OrderKey','PartKey','SuppKey','LineNumber','Quantity','ExtendedPrice','Discount','Tax','ReturnFlag',
+    nation = pd.read_table('./tpch_1/nation.tbl', index_col=False, names=['NationKey','NationName','RegionKey','Comment'], delimiter = '|').iloc[:, :-1]
+    supplier = pd.read_table('./tpch_1/supplier.tbl', index_col=False, names=['SuppKey','SuppName','Address','NationKey','Phone','Acctbl','Comment'], delimiter = '|').iloc[:, :-1]
+    customer = pd.read_table('./tpch_1/customer.tbl', index_col=False, names=['CustKey','CustName','Address','NationKey','Phone','Acctbal','MktSegment','Comment'], delimiter = '|').iloc[:, :-1]
+    orders = pd.read_table('./tpch_1/orders.tbl', index_col=False, names=['OrderKey','CustKey','OrderStatus','TotalPrice','OrderDate','OrderPriority','Clerk','ShipPriority','Comment'], delimiter = '|').iloc[:, :-1]
+    lineitem = pd.read_table('./tpch_1/lineitem.tbl', index_col=False, names=['OrderKey','PartKey','SuppKey','LineNumber','Quantity','ExtendedPrice','Discount','Tax','ReturnFlag',
     'LineStatus','ShipDate','CommitDate','ReceiptDate','ShipinStruct','ShipMode','Comment'], delimiter = '|').iloc[:, :-1]
 
     print("read successfully")
 
     nation_sample_1,supplier_sample_1,customer_sample_1,orders_sample_1,lineitem_sample_1 = process_tpch(overlap, scale, nation, supplier, customer, orders, lineitem)
-    # nation_sample_1.to_csv(r'./tpch_3_chain_5/n1.csv')
-    # supplier_sample_1.to_csv(r'./tpch_3_chain_5/s1.csv')
-    # customer_sample_1.to_csv(r'./tpch_3_chain_5/c1.csv')
-    # orders_sample_1.to_csv(r'./tpch_3_chain_5/o1.csv')
-    # lineitem_sample_1.to_csv(r'./tpch_3_chain_5/l1.csv')
+    nation_sample_1.to_csv(r'./tpch_1_chain_5/n1.csv')
+    supplier_sample_1.to_csv(r'./tpch_1_chain_5/s1.csv')
+    customer_sample_1.to_csv(r'./tpch_1_chain_5/c1.csv')
+    orders_sample_1.to_csv(r'./tpch_1_chain_5/o1.csv')
+    lineitem_sample_1.to_csv(r'./tpch_1_chain_5/l1.csv')
 
     print("1 done")
 
     nation_sample_2,supplier_sample_2,customer_sample_2,orders_sample_2,lineitem_sample_2 = process_tpch(overlap, scale, nation, supplier, customer, orders, lineitem)
-    # nation_sample_2.to_csv(r'./tpch_3_chain_5/n2.csv')
-    # supplier_sample_2.to_csv(r'./tpch_3_chain_5/s2.csv')
-    # customer_sample_2.to_csv(r'./tpch_3_chain_5/c2.csv')
-    # orders_sample_2.to_csv(r'./tpch_3_chain_5/o2.csv')
-    # lineitem_sample_2.to_csv(r'./tpch_3_chain_5/l2.csv')
+    nation_sample_2.to_csv(r'./tpch_1_chain_5/n2.csv')
+    supplier_sample_2.to_csv(r'./tpch_1_chain_5/s2.csv')
+    customer_sample_2.to_csv(r'./tpch_1_chain_5/c2.csv')
+    orders_sample_2.to_csv(r'./tpch_1_chain_5/o2.csv')
+    lineitem_sample_2.to_csv(r'./tpch_1_chain_5/l2.csv')
 
     print("2 done")
 
     nation_sample_3,supplier_sample_3,customer_sample_3,orders_sample_3,lineitem_sample_3 = process_tpch(overlap, scale, nation, supplier, customer, orders, lineitem)
-    # nation_sample_3.to_csv(r'./tpch_3_chain_5/n3.csv')
-    # supplier_sample_3.to_csv(r'./tpch_3_chain_5/s3.csv')
-    # customer_sample_3.to_csv(r'./tpch_3_chain_5/c3.csv')
-    # orders_sample_3.to_csv(r'./tpch_3_chain_5/o3.csv')
-    # lineitem_sample_3.to_csv(r'./tpch_3_chain_5/l3.csv')
+    nation_sample_3.to_csv(r'./tpch_1_chain_5/n3.csv')
+    supplier_sample_3.to_csv(r'./tpch_1_chain_5/s3.csv')
+    customer_sample_3.to_csv(r'./tpch_1_chain_5/c3.csv')
+    orders_sample_3.to_csv(r'./tpch_1_chain_5/o3.csv')
+    lineitem_sample_3.to_csv(r'./tpch_1_chain_5/l3.csv')
 
     print("3 done")
 
     nation_sample_4,supplier_sample_4,customer_sample_4,orders_sample_4,lineitem_sample_4 = process_tpch(overlap, scale, nation, supplier, customer, orders, lineitem)
-    # nation_sample_4.to_csv(r'./tpch_3_chain_5/n4.csv')
-    # supplier_sample_4.to_csv(r'./tpch_3_chain_5/s4.csv')
-    # customer_sample_4.to_csv(r'./tpch_3_chain_5/c4.csv')
-    # orders_sample_4.to_csv(r'./tpch_3_chain_5/o4.csv')
-    # lineitem_sample_4.to_csv(r'./tpch_3_chain_5/l4.csv')
+    nation_sample_4.to_csv(r'./tpch_1_chain_5/n4.csv')
+    supplier_sample_4.to_csv(r'./tpch_1_chain_5/s4.csv')
+    customer_sample_4.to_csv(r'./tpch_1_chain_5/c4.csv')
+    orders_sample_4.to_csv(r'./tpch_1_chain_5/o4.csv')
+    lineitem_sample_4.to_csv(r'./tpch_1_chain_5/l4.csv')
 
     print("4 done")
 
     nation_sample_5,supplier_sample_5,customer_sample_5,orders_sample_5,lineitem_sample_5 = process_tpch(overlap, scale, nation, supplier, customer, orders, lineitem)
-    # nation_sample_5.to_csv(r'./tpch_3_chain_5/n5.csv')
-    # supplier_sample_5.to_csv(r'./tpch_3_chain_5/s5.csv')
-    # customer_sample_5.to_csv(r'./tpch_3_chain_5/c5.csv')
-    # orders_sample_5.to_csv(r'./tpch_3_chain_5/o5.csv')
-    # lineitem_sample_5.to_csv(r'./tpch_3_chain_5/l5.csv')
+    nation_sample_5.to_csv(r'./tpch_1_chain_5/n5.csv')
+    supplier_sample_5.to_csv(r'./tpch_1_chain_5/s5.csv')
+    customer_sample_5.to_csv(r'./tpch_1_chain_5/c5.csv')
+    orders_sample_5.to_csv(r'./tpch_1_chain_5/o5.csv')
+    lineitem_sample_5.to_csv(r'./tpch_1_chain_5/l5.csv')
 
     print("5 done")
 
-    # nation_sample_1 = pd.read_csv('./tpch_3_chain_5/n1.csv' ,index_col=0)
-    # supplier_sample_1 = pd.read_csv('./tpch_3_chain_5/s1.csv' ,index_col=0)
-    # customer_sample_1 = pd.read_csv('./tpch_3_chain_5/c1.csv' ,index_col=0)
-    # orders_sample_1 = pd.read_csv('./tpch_3_chain_5/o1.csv' ,index_col=0)
-    # lineitem_sample_1 = pd.read_csv('./tpch_3_chain_5/l1.csv' ,index_col=0)
+    # nation_sample_1 = pd.read_csv('./tpch_1_chain_5/n1.csv' ,index_col=0)
+    # supplier_sample_1 = pd.read_csv('./tpch_1_chain_5/s1.csv' ,index_col=0)
+    # customer_sample_1 = pd.read_csv('./tpch_1_chain_5/c1.csv' ,index_col=0)
+    # orders_sample_1 = pd.read_csv('./tpch_1_chain_5/o1.csv' ,index_col=0)
+    # lineitem_sample_1 = pd.read_csv('./tpch_1_chain_5/l1.csv' ,index_col=0)
 
-    # nation_sample_2 = pd.read_csv('./tpch_3_chain_5/n2.csv' ,index_col=0)
-    # supplier_sample_2 = pd.read_csv('./tpch_3_chain_5/s2.csv' ,index_col=0)
-    # customer_sample_2 = pd.read_csv('./tpch_3_chain_5/c2.csv' ,index_col=0)
-    # orders_sample_2 = pd.read_csv('./tpch_3_chain_5/o2.csv' ,index_col=0)
-    # lineitem_sample_2 = pd.read_csv('./tpch_3_chain_5/l2.csv' ,index_col=0)
+    # nation_sample_2 = pd.read_csv('./tpch_1_chain_5/n2.csv' ,index_col=0)
+    # supplier_sample_2 = pd.read_csv('./tpch_1_chain_5/s2.csv' ,index_col=0)
+    # customer_sample_2 = pd.read_csv('./tpch_1_chain_5/c2.csv' ,index_col=0)
+    # orders_sample_2 = pd.read_csv('./tpch_1_chain_5/o2.csv' ,index_col=0)
+    # lineitem_sample_2 = pd.read_csv('./tpch_1_chain_5/l2.csv' ,index_col=0)
 
-    # nation_sample_3 = pd.read_csv('./tpch_3_chain_5/n3.csv' ,index_col=0)
-    # supplier_sample_3 = pd.read_csv('./tpch_3_chain_5/s3.csv' ,index_col=0)
-    # customer_sample_3 = pd.read_csv('./tpch_3_chain_5/c3.csv' ,index_col=0)
-    # orders_sample_3 = pd.read_csv('./tpch_3_chain_5/o3.csv' ,index_col=0)
-    # lineitem_sample_3 = pd.read_csv('./tpch_3_chain_5/l3.csv' ,index_col=0)
+    # nation_sample_3 = pd.read_csv('./tpch_1_chain_5/n3.csv' ,index_col=0)
+    # supplier_sample_3 = pd.read_csv('./tpch_1_chain_5/s3.csv' ,index_col=0)
+    # customer_sample_3 = pd.read_csv('./tpch_1_chain_5/c3.csv' ,index_col=0)
+    # orders_sample_3 = pd.read_csv('./tpch_1_chain_5/o3.csv' ,index_col=0)
+    # lineitem_sample_3 = pd.read_csv('./tpch_1_chain_5/l3.csv' ,index_col=0)
 
-    # nation_sample_4 = pd.read_csv('./tpch_3_chain_5/n4.csv' ,index_col=0)
-    # supplier_sample_4 = pd.read_csv('./tpch_3_chain_5/s4.csv' ,index_col=0)
-    # customer_sample_4 = pd.read_csv('./tpch_3_chain_5/c4.csv' ,index_col=0)
-    # orders_sample_4 = pd.read_csv('./tpch_3_chain_5/o4.csv' ,index_col=0)
-    # lineitem_sample_4 = pd.read_csv('./tpch_3_chain_5/l4.csv' ,index_col=0)
+    # nation_sample_4 = pd.read_csv('./tpch_1_chain_5/n4.csv' ,index_col=0)
+    # supplier_sample_4 = pd.read_csv('./tpch_1_chain_5/s4.csv' ,index_col=0)
+    # customer_sample_4 = pd.read_csv('./tpch_1_chain_5/c4.csv' ,index_col=0)
+    # orders_sample_4 = pd.read_csv('./tpch_1_chain_5/o4.csv' ,index_col=0)
+    # lineitem_sample_4 = pd.read_csv('./tpch_1_chain_5/l4.csv' ,index_col=0)
 
-    # nation_sample_5 = pd.read_csv('./tpch_3_chain_5/n5.csv' ,index_col=0)
-    # supplier_sample_5 = pd.read_csv('./tpch_3_chain_5/s5.csv' ,index_col=0)
-    # customer_sample_5 = pd.read_csv('./tpch_3_chain_5/c5.csv' ,index_col=0)
-    # orders_sample_5 = pd.read_csv('./tpch_3_chain_5/o5.csv' ,index_col=0)
-    # lineitem_sample_5 = pd.read_csv('./tpch_3_chain_5/l5.csv' ,index_col=0)
+    # nation_sample_5 = pd.read_csv('./tpch_1_chain_5/n5.csv' ,index_col=0)
+    # supplier_sample_5 = pd.read_csv('./tpch_1_chain_5/s5.csv' ,index_col=0)
+    # customer_sample_5 = pd.read_csv('./tpch_1_chain_5/c5.csv' ,index_col=0)
+    # orders_sample_5 = pd.read_csv('./tpch_1_chain_5/o5.csv' ,index_col=0)
+    # lineitem_sample_5 = pd.read_csv('./tpch_1_chain_5/l5.csv' ,index_col=0)
 
     tables_1 = [nation_sample_1, supplier_sample_1, customer_sample_1, orders_sample_1,lineitem_sample_1]
     tables_2 = [nation_sample_2, supplier_sample_2, customer_sample_2, orders_sample_2,lineitem_sample_2]
@@ -162,43 +162,43 @@ def main():
     hs_1 = hash_j(join_1)
     print("Hash success")
 
-    # f = open("./tpch_3_chain_5/q1_hs.pkl","wb")
+    # f = open("./tpch_1_chain_5/q1_hs.pkl","wb")
     # pickle.dump(hs_1,f)
     # f.close()
 
     hs_2 = hash_j(join_2)
     print("Hash success")
 
-    # f = open("./tpch_3_chain_5/q2_hs.pkl","wb")
+    # f = open("./tpch_1_chain_5/q2_hs.pkl","wb")
     # pickle.dump(hs_2,f)
     # f.close()
 
     hs_3 = hash_j(join_3)
     print("Hash success")
 
-    # f = open("./tpch_3_chain_5/q3_hs.pkl","wb")
+    # f = open("./tpch_1_chain_5/q3_hs.pkl","wb")
     # pickle.dump(hs_3,f)
     # f.close()
 
     hs_4 = hash_j(join_4)
     print("Hash success")
 
-    # f = open("./tpch_3_chain_5/q4_hs.pkl","wb")
+    # f = open("./tpch_1_chain_5/q4_hs.pkl","wb")
     # pickle.dump(hs_4,f)
     # f.close()
 
     hs_5 = hash_j(join_5)
     print("Hash success")
 
-    # f = open("./tpch_3_chain_5/q5_hs.pkl","wb")
+    # f = open("./tpch_1_chain_5/q5_hs.pkl","wb")
     # pickle.dump(hs_5,f)
     # f.close()
     
-    # hs_1 = pickle.load(open("./tpch_3_chain_5/q1_hs.pkl", "rb"))
-    # hs_2 = pickle.load(open("./tpch_3_chain_5/q2_hs.pkl", "rb"))
-    # hs_3 = pickle.load(open("./tpch_3_chain_5/q3_hs.pkl", "rb"))
-    # hs_4 = pickle.load(open("./tpch_3_chain_5/q4_hs.pkl", "rb"))
-    # hs_5 = pickle.load(open("./tpch_3_chain_5/q5_hs.pkl", "rb"))
+    # hs_1 = pickle.load(open("./tpch_1_chain_5/q1_hs.pkl", "rb"))
+    # hs_2 = pickle.load(open("./tpch_1_chain_5/q2_hs.pkl", "rb"))
+    # hs_3 = pickle.load(open("./tpch_1_chain_5/q3_hs.pkl", "rb"))
+    # hs_4 = pickle.load(open("./tpch_1_chain_5/q4_hs.pkl", "rb"))
+    # hs_5 = pickle.load(open("./tpch_1_chain_5/q5_hs.pkl", "rb"))
     # print("hash successfully loaded")
 
     # ----------------------------------- Estimation ----------------------------------- 
@@ -234,33 +234,33 @@ def main():
     # print("Exact union size:")
     # print(exact_union.shape[0])
 
-     # ----------------------------------- sample from disjoint ----------------------------------- 
+    # ----------------------------------- sample from disjoint ----------------------------------- 
     exact_S, exact_time = exact_sample_from_disjoint([join_1, join_2, join_3, join_4, join_5], n, [hs_1, hs_2, hs_3, hs_4, hs_5])
     print(exact_time)
-    exact_S.to_csv(r'./tpch_3_chain_5/exact_sample_10000.csv')
-    with open('./tpch_3_chain_5/exact_time_10000.txt', 'w') as f:
+    exact_S.to_csv(r'./tpch_1_chain_5/exact_sample_10000.csv')
+    with open('./tpch_1_chain_5/exact_time_10000.txt', 'w') as f:
         f.write(json.dumps(exact_time))
 
 
     olken_S, olken_time = olken_sample_from_disjoint([join_1, join_2, join_3, join_4, join_5], n, [hs_1, hs_2, hs_3, hs_4, hs_5])
     print(olken_time)
-    olken_S.to_csv(r'./tpch_3_chain_5/olken_sample_10000.csv')
-    with open('./tpch_3_chain_5/olken_time_10000.txt', 'w') as f:
+    olken_S.to_csv(r'./tpch_1_chain_5/olken_sample_10000.csv')
+    with open('./tpch_1_chain_5/olken_time_10000.txt', 'w') as f:
         f.write(json.dumps(olken_time))
 
     # ----------------------------------- sample from union bernoulli  ----------------------------------- 
 
-    # olken_u_b_S, olken_u_b_time = olken_olken_sample_union_bernoulli([join_1, join_2, join_3, join_4, join_5], n, [hs_1, hs_2, hs_3, hs_4, hs_5])
-    # print(olken_u_b_time)
-    # olken_u_b_S.to_csv(r'./tpch_3_chain_5/olken_u_b_sample_1000000.csv')
-    # with open('./tpch_3_chain_5/olken_u_b_time_1000000.txt', 'w') as f:
-    #     f.write(json.dumps(olken_u_b_time))
+    olken_u_b_S, olken_u_b_time = olken_olken_sample_union_bernoulli([join_1, join_2, join_3, join_4, join_5], n, [hs_1, hs_2, hs_3, hs_4, hs_5])
+    print(olken_u_b_time)
+    olken_u_b_S.to_csv(r'./tpch_1_chain_5/olken_u_b_sample_10000.csv')
+    with open('./tpch_1_chain_5/olken_u_b_time_10000.txt', 'w') as f:
+        f.write(json.dumps(olken_u_b_time))
 
-    # exact_u_b_S, exact_u_b_time = exact_olken_sample_union_bernoulli([join_1, join_2, join_3, join_4, join_5], n, [hs_1, hs_2, hs_3, hs_4, hs_5])
-    # print(exact_u_b_time)
-    # exact_u_b_S.to_csv(r'./tpch_3_chain_5/exact_u_b_sample_1000000.csv')
-    # with open('./tpch_3_chain_5/exact_u_b_time_1000000.txt', 'w') as f:
-    #     f.write(json.dumps(exact_u_b_time))
+    exact_u_b_S, exact_u_b_time = exact_olken_sample_union_bernoulli([join_1, join_2, join_3, join_4, join_5], n, [hs_1, hs_2, hs_3, hs_4, hs_5])
+    print(exact_u_b_time)
+    exact_u_b_S.to_csv(r'./tpch_1_chain_5/exact_u_b_sample_10000.csv')
+    with open('./tpch_1_chain_5/exact_u_b_time_10000.txt', 'w') as f:
+        f.write(json.dumps(exact_u_b_time))
 
 
     # baseline
